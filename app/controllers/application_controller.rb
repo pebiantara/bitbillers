60f766db3f50bbbfd90f95f44279c3a345b5ecb2
pebiantara <pebiantara@gmail.com>
2016-02-29 01:05:14 +0700

    admin stylesheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :selected_layout
+  layout :selected_layout
   before_action :authorize
 
   def selected_layout
